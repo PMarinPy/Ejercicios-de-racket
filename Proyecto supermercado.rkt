062c3fff724756a344ab0b4651e2779b66566bad
PMarinPy <pablo.marin1@utp.edu.co>
2024-05-06 21:41:11 -0500

    A

--- Proyecto supermercado.rkt
+++ Proyecto supermercado.rkt
@@ -36,4 +36,96 @@
         (string-set! strf ind (desencarac (string-ref str ind) sem))
         (desencrip str sem (+ ind 1) strf))
       strf))
-;
+
+;CREACIÓN DE FUNCIONES PARA MANIPULACIÓN DE LA BASE DE DATOS.
+;-------------------------------
+;FUNCIÓN PARA 
+
+;FUNCIÓN PARA CREAR EL SUPERMERCADO
+(define (crearsuper)
+  (define basedatos 0)
+  (define nombre 0)
+  (display "¿Cúal es el nombre de su supermercado?\n")
+  (set! nombre (read-line))
+  (printf "¿Cuántos productos tendrá ~a?\n" nombre)
+  (set! basedatos (make-vector (read) (make-vector 8 0)))
+  (menu basedatos nombre))
+;FUNCIÓN PARA MOSTRAR EL MENÚ
+(define (menu basedatos nombre)
+  (display "     @@@@@                                         
+         .@                                         
+          @@                                        
+          @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  
+           @@        @@       @@        @@       @@ 
+           @@        @@       @@        @@      @@  
+            @@       @@       @@       @@       @@  
+            @@       @@       @@       @@       @@  
+             @       @@       @@       @@      @@   
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@   
+             @@#      @@      @@      %@%     @@:   
+              @@      @@      @@      @@      @@    
+              @@      @@      @@      @@      @@    
+               @@     :@:     @@      @@     #@     
+               @@      @@     @@      @@     @@     
+             @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@      
+           @@                                       
+           @@                                       
+            @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@      
+                 @@@    @@          @@@   .@@       
+                 @@     @@          @@     @@       
+                  @@   .@#           @@   -@*       
+                    @@@%               @@@#   \n")
+  (printf "¡BIENVENIDO AL SUPERMERCADO ~a!\n" nombre)
+  (display "¿Qué desea hacer?\n")
+  (displayln "+------------------------------------------------+")
+  (displayln "|                   Menú Principal               |")
+  (displayln "+------------------------------------------------+")
+  (displayln "| Opción |            Descripción                |")
+  (displayln "+------------------------------------------------+")
+  (displayln "|   1    |  Ingreso de un producto nuevo         |")
+  (displayln "|   2    |  Actualización de datos de un producto|")
+  (displayln "|   3    |  Listado de productos por tipo        |")
+  (displayln "|   4    |  Listado de productos por valor       |")
+  (displayln "|   5    |  Listado general de productos         |")
+  (displayln "|   6    |  Salir                                |")
+  (displayln "+------------------------------------------------+")
+  )
+;FUNCIÓN PARA LLAMAR A LAS FUNCIONES CORRESPONDIENTES.
+(define (usarmenu op basedatos nombre)
+  (cond
+    [(= op 1)(begin
+               
+               (menu basedatos nombre))]
+    [(= op 2)(begin
+               
+               (menu basedatos nombre))]
+    [(= op 3)(begin
+               
+               (menu basedatos nombre))]
+    [(= op 4)(begin
+               
+               (menu basedatos nombre))]
+    [(= op 5)(begin
+               
+               (menu basedatos nombre))]
+    [(= op 6)("¡MUCHAS GRACIAS POR USAR NUESTRA BASE DE DATOS!")]
+    ))
+
+(crearsuper)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
